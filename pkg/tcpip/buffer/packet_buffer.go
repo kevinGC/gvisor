64f3fe5ca6b5ddc2d2d80c2065faaa78db31b3da
Kevin Krakauer <krakauer@google.com>
2019-10-19 21:15:10 -0700

    Use PacketBuffers instead of VectorisedViews.
    Upper layers sometimes need access to lower layer headers (e.g. packet sockets
    providing link headers). iptables will also need access to information from
    each header. PacketBuffers keep all that data together and accessible.

---
+++ pkg/tcpip/buffer/packet_buffer.go
@@ -0,0 +1,32 @@
+// Copyright 2019 The gVisor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package buffer
+
+// A PacketBuffer contains all the data of a network packet.
+type PacketBuffer struct {
+	// Data holds the payload of the packet. For inbound packets, it also holds
+	// all the headers.
+	Data VectorisedView
+
+	// Headers holds the link, network, and transport headers of outbound
+	// packets.
+	Headers Prependable
+
+	// All of the below are slices within Data.
+	LinkHeader      View
+	NetworkHeader   View
+	TransportHeader View
+	Payload         View
+}
