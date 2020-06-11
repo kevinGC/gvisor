576f485ec007bb5859631f144972c3a13017c5c7
Kevin Krakauer <krakauer@google.com>
2020-06-10 21:19:59 -0700

    iptables: SO_ORIGINAL_DST
    Istio uses this to get the original destination of redirected packets.

--- test/iptables/iptables_test.go
+++ test/iptables/iptables_test.go
@@ -321,3 +321,15 @@ func TestInputSource(t *testing.T) {
 func TestInputInvertSource(t *testing.T) {
 	singleTest(t, FilterInputInvertSource{})
 }
+
+func TestNATPreOriginalDst(t *testing.T) {
+	singleTest(t, NATPreOriginalDst{})
+}
+
+func TestNATOutOriginalDst(t *testing.T) {
+	singleTest(t, NATOutOriginalDst{})
+}
+
+func TestNATPreOriginalDstUnchanged(t *testing.T) {
+	singleTest(t, NATPreOriginalDstUnchanged{})
+}
